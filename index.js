54229a38eda834c9e968802b93f9cc65e917be81
Pharma2022 <111680056+Pharma2022@users.noreply.github.com>
2022-11-28 21:43:29 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("btn").addEventListener("click",function gfgMenu() {
+            const GFG = document.querySelector('.nav');
+  
+            if (GFG.style.display === "none") {
+                GFG.style.display = "block";
+            }
+            else {
+                GFG.style.display = "none";
+            }
+        })
+    
+    // javascript
